189885fcbfb979d6ef03b8ab36a7ba6d2c94b73b
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-07-02 01:53:17 -0700

    Added Dotfiles

---
+++ dotfiles.sh
@@ -0,0 +1,19 @@
+# Define your dotfiles as source→target pairs
+dotfiles=(
+  "$HOME/.dotfiles/i3:$HOME/.config/i3"
+  "$HOME/.dotfiles/gh:$HOME/.config/gh"
+  "$HOME/.dotfiles/mpv:$HOME/.config/mpv"
+  "$HOME/.dotfiles/nvim:$HOME/.config/nvim"
+  "$HOME/.dotfiles/qimgv:$HOME/.config/qimgv"
+  "$HOME/.dotfiles/wezterm:$HOME/.config/wezterm"
+  "$HOME/.dotfiles/.bashrc:$HOME/.bashrc"
+  "$HOME/.dotfiles/.tmux.conf:$HOME/.tmux.conf"
+)
+
+echo "Linking dotfiles..."
+for entry in "${dotfiles[@]}"; do
+  src="${entry%%:*}"
+  tgt="${entry##*:}"
+  echo "Linking $src → $tgt"
+  ln -sf "$src" "$tgt"
+done
