f4313c412c570313def944929efc9d8a2f26ffd3
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-12-29 06:41:54 +0000

    Added Dots

--- dots.sh
+++ dots.sh
@@ -1,18 +1,24 @@
 dotfiles=(
+    "$HOME/.dotfiles/GIMP:$HOME/.config/GIMP"
+    "$HOME/.dotfiles/btop:$HOME/.config/btop"
+    "$HOME/.dotfiles/darktable:$HOME/.config/darktable"
     "$HOME/.dotfiles/dunst:$HOME/.config/dunst"
     "$HOME/.dotfiles/gh:$HOME/.config/gh"
+    "$HOME/.dotfiles/git:$HOME/.config/git"
     "$HOME/.dotfiles/i3:$HOME/.config/i3"
     "$HOME/.dotfiles/lazygit:$HOME/.config/lazygit"
-    "$HOME/.dotfiles/mpv:$HOME/.config/mpv"
-    "$HOME/.dotfiles/nvim:$HOME/.config/nvim"
     "$HOME/.dotfiles/paru:$HOME/.config/paru"
     "$HOME/.dotfiles/pikaur:$HOME/.config/pikaur"
     "$HOME/.dotfiles/qimgv:$HOME/.config/qimgv"
     "$HOME/.dotfiles/rofi:$HOME/.config/rofi"
     "$HOME/.dotfiles/wezterm:$HOME/.config/wezterm"
-    "$HOME/.dotfiles/git:$HOME/.config/git"
+
+    "$HOME/.dotfiles/okularrc:$HOME/.config/okularrc"
+    "$HOME/.dotfiles/okularpartrc:$HOME/.config/okularpartrc"
+
     "$HOME/.dotfiles/.bashrc:$HOME/.bashrc"
     "$HOME/.dotfiles/.tmux.conf:$HOME/.tmux.conf"
+    "$HOME/.dotfiles/.vimrc:$HOME/.vimrc"
 )
 
 echo "Linking dotfiles..."
